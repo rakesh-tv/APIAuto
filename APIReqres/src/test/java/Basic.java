ede17db909b674603a9b48e38724c0a2c805b901
rakeshgithub123 <rockmail.yadav@gmail.com>
2021-05-13 13:37:51 +0530

    Added info in Readme and gitignore

--- APIReqres/src/test/java/Basic.java
+++ APIReqres/src/test/java/Basic.java
@@ -1,10 +1,4 @@
 import io.restassured.RestAssured;
-import io.restassured.builder.*;
-import io.restassured.http.ContentType;
-import io.restassured.http.Method;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import io.restassured.specification.*;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import pojo.User;
@@ -28,26 +22,19 @@ public class Basic extends TestData {
     @Test(dataProvider = "valuesForGetRequestBody")
     public void testGetRequest(String name, String job){
         RestAssured.baseURI = env.get("baseURL").toString();
-        RequestSpecification reqSpec = new RequestSpecBuilder()
-                                            .setContentType("application/json")
-                                            .setBaseUri(baseURI).build();
-        ResponseSpecification resSpec = new ResponseSpecBuilder()
-                                            .expectContentType(ContentType.JSON)
-                                            .expectStatusCode(201).build();
 
         User user = new User();
         user.setName(name);
         user.setJob(job);
 
-        User resAsUserClass = given().
-                                    spec(reqSpec).
+        User resAsUserClass =   given().
+                                    contentType("application/json").
                                     body(user).
-                              when().
+                                when().
                                     post("/api/users").
-                              then().
-                                    spec(resSpec).extract().response().as(User.class);
+                                    then().
+                                statusCode(201).extract().response().as(User.class);
 
-//        JsonPath jsonRes = new JsonPath(res.asString());
         Assert.assertEquals(resAsUserClass.getName(),"API");
         id = resAsUserClass.getId();
     }
@@ -57,24 +44,17 @@ public class Basic extends TestData {
 
         RestAssured.baseURI = env.get("baseURL").toString();
 
-        RequestSpecification reqSpec = new RequestSpecBuilder()
-                                            .setContentType("application/json")
-                                            .setBaseUri(baseURI).build();
-        ResponseSpecification resSpec = new ResponseSpecBuilder()
-                                            .expectContentType(ContentType.JSON)
-                                            .expectStatusCode(200).build();
-
         User user = new User();
         user.setName(name);
         user.setJob(job);
 
-        User resAsUserClass = given().
-                                    spec(reqSpec).
+        User resAsUserClass =   given().
+                                    contentType("application/json").
                                     body(user).
-                              when().
+                                when().
                                     put("/api/users/"+id).
-                              then().
-                                    spec(resSpec).extract().response().as(User.class);
+                                then().
+                                    statusCode(200).extract().response().as(User.class);
 
         Assert.assertEquals(resAsUserClass.getJob(),job);
 
