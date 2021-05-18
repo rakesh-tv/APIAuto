6422d8e5e1e162236a1d0cd17a3d35248ea06dc0
rakeshgithub123 <rockmail.yadav@gmail.com>
2021-05-18 14:53:57 +0530

    Added test for comments

--- APIReqres/src/test/java/requests/Comment.java
+++ APIReqres/src/test/java/requests/Comment.java
@@ -2,6 +2,7 @@ package requests;
 
 import Utils.Util;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import pojo.Comments;
 
@@ -13,22 +14,23 @@ import static io.restassured.RestAssured.given;
 
 public class Comment {
 
-    public void addNewComment(Integer id, String commentBody, Integer postId){
+    public Response addNewComment(Integer id, String commentBody, Integer postId){
         RestAssured.baseURI = Util.getBaseURL();
 
         Comments newPost = Comments.builder().id(id).body(commentBody).postId(postId).build();
 
-        Comments comment =   given().
+        Response response =   given().
                                     contentType("application/json").
                                     body(newPost).
                                 when().
                                     post("/comments").
                                 then().
-                                    statusCode(201).extract().response().as(Comments.class);
+                                    extract().response();
+        return response;
 
     }
 
-    public Comments[] getComments(){
+    public Comments[] getAllComments(){
         RestAssured.baseURI = Util.getBaseURL();
 
         Comments[] comments =   given().
@@ -41,19 +43,45 @@ public class Comment {
 
     }
 
-    public Comments updateComment(Integer commentId, String commentBody, Integer postId){
+    public Comments getComment(Integer id){
+        RestAssured.baseURI = Util.getBaseURL();
+
+        Comments comment =   given().
+                    contentType("application/json").
+                when().
+                    get("/comments/" + id.toString()).
+                then().
+                    statusCode(200).extract().response().as(Comments.class);
+        return comment;
+
+    }
+
+    public Response updateComment(Integer commentId, String commentBody, Integer postId){
         RestAssured.baseURI = Util.getBaseURL();
 
         Comments newPost = Comments.builder().id(commentId).body(commentBody).postId(postId).build();
 
-        Comments comment =   given().
+        Response response =   given().
                     contentType("application/json").
                     body(newPost).
                 when().
                     put("/comments/"+commentId.toString()).
                 then().
-                    statusCode(200).extract().response().as(Comments.class);
-        return comment;
+                    extract().response();
+        return response;
+
+    }
+
+    public Response deleteComment(Integer id){
+        RestAssured.baseURI = Util.getBaseURL();
+
+        Response response =   given().
+                    contentType("application/json").
+                when().
+                    delete("/comments/" + id.toString()).
+                then().
+                    extract().response();
+        return response;
 
     }
 
